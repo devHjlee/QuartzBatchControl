420f315c67569fe7ffecc078f80d1a394daccde1
lhj <dlgudwo11@gmail.com>
2025-05-15 19:49:53 +0900

    init commit

---
+++ QuartzBatchControl-Backend/src/main/java/com/quartzbatchcontrol/QuartzBatchControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quartzbatchcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzBatchControlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzBatchControlBackendApplication.class, args);
+    }
+
+}
